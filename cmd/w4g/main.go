e495bcb75809d126df873ebe33581be9f5a884cf
Christopher Kleine <chris@suletuxe.de>
2022-05-27 04:17:51 +0200

    Added drone.yml and git version for version info

--- cmd/w4g/main.go
+++ cmd/w4g/main.go
@@ -8,11 +8,15 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+var (
+	GitCommit string
+)
+
 func main() {
 	app := &cli.App{
 		Name:                 "w4g",
 		Usage:                "Chris' version of the WASM-4 CLI",
-		Version:              "0.0.1",
+		Version:              GitCommit,
 		Action:               commands.SurfAction,
 		EnableBashCompletion: true,
 		Authors: []*cli.Author{
